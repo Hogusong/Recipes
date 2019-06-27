9bdaa60fbfefd463cf54137df81e610cf16b113e
“Young <hogusong1@gmail.com>
2019-06-27 11:59:12 -0400

    render the detail of the ingredients of the selected recipe.

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -46,6 +46,52 @@ export const renderRecipe = (recipe) => {
         </svg>
       </button>
     </div>
+
+    <div class="recipe__ingredients">
+      <ul class="recipe__ingredient-list">
+        ${renderIngredients(recipe.ingredients)}
+      </ul>
+
+      <button class="btn-small recipe__btn">
+        <svg class="search__icon">
+          <use href="img/icons.svg#icon-shopping-cart"></use>
+        </svg>
+        <span>Add to shopping list</span>
+      </button>
+    </div>
+
+    <div class="recipe__directions">
+      <h2 class="heading-2">How to cook it</h2>
+      <p class="recipe__directions-text">
+        This recipe was carefully designed and tested by
+        <span class="recipe__by">The Pioneer Woman</span>. Please check out directions at their website.
+      </p>
+      <a class="btn-small recipe__btn" href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/" target="_blank">
+        <span>Directions</span>
+        <svg class="search__icon">
+          <use href="img/icons.svg#icon-triangle-right"></use>
+        </svg>
+      </a>
+    </div>
   `;
   dom.recipeUI.insertAdjacentHTML('beforeend', markup);
+}
+
+const renderIngredients = ingredients => {
+  let markup = '';
+  ingredients.forEach(ing => {
+    markup += `
+        <li class="recipe__item">
+          <svg class="recipe__icon">
+            <use href="img/icons.svg#icon-check"></use>
+          </svg>
+          <div class="recipe__count">${ing.count}</div>
+          <div class="recipe__ingredient">
+            <span class="recipe__unit">${ing.unit}</span>
+            ${ing.ingredient}
+          </div>
+        </li>
+      `
+  });
+  return markup
 }
