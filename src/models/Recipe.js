ed82ecd3e0752d6c364b3c76c2d729c2f7df7401
“Young <hogusong1@gmail.com>
2019-06-27 01:03:31 -0400

    parse ingredient into count, unit and ingredient after convert units to standard format.

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -12,6 +12,68 @@ export default class Recipe {
     this.author = res.data.recipe.publisher;
     this.url = res.data.recipe.surce_url;
     this.img = res.data.recipe.image_url;
-    this.ingredients = res.data.recipe.ingredients;
+    this.ingredients = this.parseIngredients(res.data.recipe.ingredients);
+  }
+
+  calcTime() {
+    // assuming that we need 15 minutes for each 4 imgredients
+    const numOfIng = this.ingredients.length;
+    this.time = Math.ceil(numOfIng / 4) * 15
+  }
+
+  calcServings() {
+    // assuming that each recipe is good to server for 4.
+    this.servings = 4;
+  }
+
+  parseIngredients(ingredients) {
+    // create units dictionary to convert unit.
+    const units = {
+      'tablespoons': 'tbsp',  'tablespoon': 'tbsp',
+      'teaspoons': 'tsp',     'teaspoon': 'tsp',
+      'ounces': 'oz',  'ounce': 'oz',
+      'cups': 'cup',   'pounds': 'pound',
+      'kg': 'kg',      'g': 'g'
+    };
+    const newIngredients = ingredients.map(ing => {
+      let ingredient = ing.toLowerCase();
+
+      // replace all units to standard format.
+      Object.keys(units).forEach(u => {
+        ingredient = ingredient.replace(u, unit[u]);
+      });
+
+      // remove all inside parentheses including parentheses
+      ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+      ingredient = ingredient.replace(',', '');
+
+      // parse ingredient into count, unit and ingredient
+      const arrIng = ingredient.split(' ');
+      let index, count, unit;
+      for (let i = 0; i < arrIng.length; i++) {
+        if ('123456789'.includes(arrIng[i][0])) continue;
+        index = i;
+        unit = arrIng[i];
+        break;
+      }
+      
+      if (index < 1) {
+        count = 1;
+      } else {
+        const arrCount = arrIng.slice(0, index);
+        arrCount[0] = arrCount[0].replace('-', '+');
+        count = eval(arrCount.join('+'));
+      }
+
+      if (!Object.values(units).includes(unit)) {
+        unit = ' '
+      } else {
+        index++;
+      }
+
+      ingredient = arrIng.slice(index).join(' ')
+      return { count, unit, ingredient }
+    });
+    return newIngredients;
   }
 }
