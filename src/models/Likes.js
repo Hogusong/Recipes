7ceb0d32cd5d2192ed2f0903428fc703b94f8380
“Young <hogusong1@gmail.com>
2019-06-27 18:22:47 -0400

    create a class LIKES to control Likes Object.

---
+++ src/models/Likes.js
@@ -0,0 +1,25 @@
+export default class Likes {
+  constructor() {
+    this.likes = []
+  }
+
+  addLike(item) {
+    const index = this.likes.findIndex(el => el.id === item.id);
+    if (index < 0) {
+      this.likes.push(item)
+    }
+  }
+
+  deleteLike(id) {
+    const index = this.likes.findIndex(el => el.id === id);
+    if (index >= 0) this.likes.splice(index, 1);
+  }
+
+  isLiked(id) {
+    return this.likes.find(el => el.id === id);
+  }
+
+  getNumLikes() {
+    return this.likes.length;
+  }
+}
